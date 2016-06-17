e7d883be89d1442d730a4e46dd58425962036d77
Melanie27 <melaniemcganney@gmail.com>
2016-06-17 11:37:41 -0700

    still working on comments

--- Blocstagram/Blocstagram/Media.m
+++ Blocstagram/Blocstagram/Media.m
@@ -26,6 +26,7 @@
         
         NSDictionary *captionDictionary = mediaDictionary[@"caption"];
         
+        
         // Caption might be null (if there's no caption)
         if ([captionDictionary isKindOfClass:[NSDictionary class]]) {
             self.caption = captionDictionary[@"text"];
@@ -35,9 +36,19 @@
         
         NSMutableArray *commentsArray = [NSMutableArray array];
         
-        for (NSDictionary *commentDictionary in mediaDictionary[@"comments"][@"data"]) {
-            Comment *comment = [[Comment alloc] initWithDictionary:commentDictionary];
-            [commentsArray addObject:comment];
+        for (NSDictionary *commentDictionary in mediaDictionary[@"comments"]) {
+            
+            //Comment *comment = [[Comment alloc] initWithDictionary:commentDictionary];
+            if ([commentDictionary isKindOfClass:[NSDictionary class]]) {
+                self.comments = captionDictionary[@"count"];
+                NSLog(@"THERE IS AT LEAST ONE COMMENT");
+            } else {
+                self.comments = @"";
+                NSLog(@"Nope");
+            }
+            
+            NSLog(@"%@",commentDictionary);
+            //[commentsArray addObject:comment];
         }
         
         self.comments = commentsArray;
