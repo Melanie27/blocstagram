103aa86577a6c51dac2d42928125d8b795629548
Melanie27 <melaniemcganney@gmail.com>
2016-06-20 18:25:26 -0700

    checkpoint 41 started

---
+++ Blocstagram/Blocstagram/CameraViewController.m
@@ -0,0 +1,52 @@
+//
+//  CameraViewController.m
+//  Blocstagram
+//
+//  Created by MELANIE MCGANNEY on 6/20/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "CameraViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import "CameraToolbar.h"
+
+@interface CameraViewController () <CameraToolbarDelegate>
+
+@property (nonatomic, strong) UIView *imagePreview;
+
+@property (nonatomic, strong) AVCaptureSession *session;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
+@property (nonatomic, strong) AVCaptureStillImageOutput *stillImageOutput;
+
+@property (nonatomic, strong) NSArray *horizontalLines;
+@property (nonatomic, strong) NSArray *verticalLines;
+@property (nonatomic, strong) UIToolbar *topView;
+@property (nonatomic, strong) UIToolbar *bottomView;
+
+@property (nonatomic, strong) CameraToolbar *cameraToolbar;
+
+@end
+
+@implementation CameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
